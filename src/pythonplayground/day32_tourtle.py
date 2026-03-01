0294997bc029999465ec1b4b0c55e86f6e22b323
Michael Klippel <digitaluselessbox@users.noreply.github.com>
2026-03-01 22:57:59 +0100

    a course test. paint a house with tourtle

---
+++ src/pythonplayground/day32_tourtle.py
@@ -0,0 +1,103 @@
+# Instructions:
+
+# Using filled shapes, draw a simple house. Your house should have at least:
+
+# A square or rectangle for the main building.
+# A triangle for the roof.
+# At least one window or door, using any shape.
+# Feel free to add additional details like a chimney, pathway, or surrounding trees using shapes and colors.
+
+# Tips:
+
+# Use begin_fill() and end_fill() to fill in each shape.
+# Change colors between shapes to make your house more vibrant.
+# Use penup() and pendown() to move between different parts of your drawing without drawing lines.
+
+from turtle import *
+
+bgcolor("lightblue")  # Set the background color
+
+# Set up the turtle
+speed(5)  # Set the speed of the turtle
+color("black", "white")  # Set pen color and fill color
+
+LINE_COLOR_DEFAULT = "black"
+
+
+
+def set_colors(line_color=LINE_COLOR_DEFAULT, fill_color=None):
+    color_args = [line_color]
+    if fill_color:
+        color_args.append(fill_color)
+    color(*color_args)
+
+
+def move_pen_tip(x:int, y:int):
+    penup()
+    goto(x, y)
+    pendown()
+
+
+def draw_circle(radius, line_color=LINE_COLOR_DEFAULT, fill_color=None):
+    set_colors(line_color, fill_color)
+
+    begin_fill()
+    circle(radius)
+    end_fill()
+
+
+def draw_square(size, line_color = LINE_COLOR_DEFAULT, fill_color=None):
+    set_colors(line_color, fill_color)
+
+
+    begin_fill()
+    for _ in range(4):
+        forward(size)
+        right(90)
+    end_fill()
+
+
+def draw_triangle(size, degree, line_color=LINE_COLOR_DEFAULT, fill_color=None):
+    set_colors(line_color, fill_color)
+
+    begin_fill()
+    for _ in range(3):
+        forward(size)
+        right(degree)
+    end_fill()
+
+
+def draw_rectangle(width, height, line_color=LINE_COLOR_DEFAULT, fill_color=None):
+    set_colors(line_color, fill_color)
+
+    begin_fill()
+    for _ in range(2):
+        forward(width)
+        right(90)
+        forward(height)
+        right(90)
+    end_fill()
+
+
+# Draw the main building
+move_pen_tip(0, 0)  # Move to the starting position
+draw_rectangle(300, 200, "#000000", "#b12929")
+
+# Draw the roof
+move_pen_tip(-30, 0)  # Move to the top of the building
+draw_triangle(360, -120, "#353d35")
+
+# Draw the door
+move_pen_tip(50, -30)  # Move to the position for the door
+draw_rectangle(80, 170, "#000000", "#C5905B")
+
+# Draw a window
+move_pen_tip(150, -50)  # Move to the position for the window
+draw_rectangle(100, 60, "#000000", "#3088D0")
+
+# draw another round window in the roof
+move_pen_tip(150, 100)  # Move to the position for the round window
+draw_circle(40, "#000000", "#3088D0")
+
+
+done()
